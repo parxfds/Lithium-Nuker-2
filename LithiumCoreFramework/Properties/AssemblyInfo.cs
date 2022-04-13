22311c4111bd4830a8a2eebf7be304a4d8b8ce52
Inc <42002057+parxfds@users.noreply.github.com>
2022-04-14 00:48:58 +0100

    Update AssemblyInfo.cs

--- LithiumCoreFramework/Properties/AssemblyInfo.cs
+++ LithiumCoreFramework/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LithiumCoreFramework")]
+[assembly: AssemblyTitle("LunaNuker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LithiumCoreFramework")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("Luna")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
